c821184b272aa05913ba2faca2bac754041f8e58
ninjaaung <ninja.aung02@gmail.com>
2020-04-14 14:12:14 -0700

    [Update] book_site/urls.py, added home /

--- books/urls.py
+++ books/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name="home"),
     path('<int:book_id>/', views.detail, name='detail')
 ]
